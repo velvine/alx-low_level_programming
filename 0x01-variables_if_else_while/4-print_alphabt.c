efbcd0ca610983e85667ce1e45da1de0950bf7bc
Ian Tanui <72918706+iantanui@users.noreply.github.com>
2022-03-14 23:03:47 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,23 @@
-4-print_alphabt.c
+#include <stdio.h>
+
+/**
+ *  main -Program entry point
+ *
+ *  Retur: on success. Error code otherwise
+ */
+int main (void)
+{
+	char ch = 'a';
+
+	while(ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
